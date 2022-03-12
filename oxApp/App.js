4aa5a2af23452b02ab064468a3cbae48baf45b9a
Eliseu <eliseu.imasd@gmail.com>
2022-03-12 13:37:06 -0300

    ...

--- oxApp/App.js
+++ oxApp/App.js
@@ -1,14 +1,14 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import api from './api';
 import {
   LineChart,
-  // BarChart,
-  // PieChart,
-  // ProgressChart,
-  // ContributionGraph,
-  // StackedBarChart
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
 } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
 
@@ -18,24 +18,52 @@ import { Dimensions } from "react-native";
 export default function App() {
 
   const [bat, setBat] = React.useState([]);  
-  const dados = []; 
+  // const dados = JSON.parse(); 
 
   React.useEffect(() => {
     api.get("/batimentos").then((response) => {
       setBat(response.data)
-      console.log(response.data, "dados")
+      // console.log(response.data, "dados");
     });
   }, []);
 
   const screenWidth = Dimensions.get("window").width - 30;
 
+  const dataArrayDados = [];
+
+  // bat.map(bats => bats.O2);
+
+  const responseData =  [];
+  dataArrayDados.push(bat.map(bats => bats.O2)); 
+  console.log(responseData, "Linda");
+  console.log(dataArrayDados, 'Elias');
+  
+  dataArrayDados[0].forEach(function(cor) {
+    responseData.push(parseInt(cor))
+  });
+
+  console.log(responseData, "Agora Vai")
+
+
+  // Object.keys(dataArrayDados).forEach(function(item){
+  //   console.log(O2[item]);
+  //  });
+
+  // for (const item in dataArrayDados[0]){
+    // console.log(item, "tentando mostrar valor")
+  // }
+  // function filtradado(value){
+  //   for(repon)
+  // }
+
+
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00"],
     datasets: [
       {
-        data: [10, 20, 30, 40, 50, 60, 70, 80, 90 ,100, 110, 120, 130, 140, 150],
+        data: [1,2,3,4,5,5,5,1],
         color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-        strokeWidth: 2 // optional
+        strokeWidth: 5 // optional
       }
     ],
     legend: ["Quantidade de O2"] // optional
@@ -47,12 +75,10 @@ export default function App() {
     backgroundGradientTo: "#08130D",
     backgroundGradientToOpacity: 0.5,
     color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    strokeWidth: 2, // optional, default 3
+    strokeWidth: 3, // optional, default 3
     barPercentage: 0.5,
     useShadowColorFromDataset: false // optional
   };
-
-  
 
   return (
     <View style={styles.container}>
@@ -61,18 +87,14 @@ export default function App() {
       <LineChart
         data={data}
         width={screenWidth}
-        height={256}
-        verticalLabelRotation={30}
+        height={350}
         chartConfig={chartConfig}
-        bezier
       />
-
     </View>
   );
 
 
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -85,3 +107,5 @@ const styles = StyleSheet.create({
     color: '#fff'
   },  
 });
+
+
